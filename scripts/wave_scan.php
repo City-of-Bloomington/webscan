d1c0a9e499a31af3c311073a342f088106849190
Cliff Ingham <inghamn@bloomington.in.gov>
2026-04-09 14:41:30 -0400

    Send PDF files to Grackle during WAVE scan
    Rather than checking all files against all webpages, we want to rely on just looking at pages as they are edited.  Links to PDF files in the content should be checked against Grackle scores.  Any PDF links we do not have a score for should be sent to Grackle.
    
    Updates #10

--- scripts/wave_scan.php
+++ scripts/wave_scan.php
@@ -7,16 +7,18 @@ declare (strict_types=1);
 
 use Application\Content\ContentRepository;
 use Application\Database;
+use Application\GrackleGateway;
 use Application\WaveGateway;
 
 include '../src/Web/bootstrap.php';
 
 $content = new ContentRepository();
 $api     = new WaveGateway(['api_key' => WAVE_API_KEY]);
+$grackle = new GrackleGateway($GRACKLE);
 $drupal  = Database::getConnection('drupal');
-$wave    = Database::getConnection('default');
-$del     = $wave->prepare('delete from reports where nid=?');
-$ins     = $wave->prepare("insert reports (nid,path,error,contrast,alert,report) values(?,?,?,?,?,?)");
+$webscan = Database::getConnection('default');
+$del     = $webscan->prepare('delete from reports where nid=?');
+$ins     = $webscan->prepare("insert reports (nid,path,error,contrast,alert,report) values(?,?,?,?,?,?)");
 
 $sql     = "select n.nid,
                    n.type,
@@ -55,4 +57,72 @@ foreach ($query->fetchAll(\PDO::FETCH_ASSOC) as $node) {
         $json['categories']['alert'   ]['count'],
         json_encode($json)
     ]);
+
+    $links = links((int)$node['nid'], $content);
+    $query = $webscan->prepare('select * from grackle_results where url=?');
+    foreach ($links as $url) {
+        $query->execute([$url]);
+        $score = $query->fetchAll(\PDO::FETCH_ASSOC);
+        if (!count($score)) {
+            // Send to grackle
+            if (substr($url, 0, 46) == 'https://bloomington.in.gov/sites/default/files') {
+                update_grackle_score($url, $grackle);
+            }
+        }
+    }
+}
+
+
+function links(int $nid, ContentRepository $content): array
+{
+    $batch   = [];
+    $regex   = 'href="([^"]+\\.pdf)"';
+    $res     = $content->page_content($nid, '.pdf');
+    foreach ($res as $r) {
+        switch ($r['field']) {
+            // HTML content
+            case 'node__body':
+            case 'node__field_aside':
+            case 'node__field_details':
+                // Find the URLs for PDF links in the HTML
+                $matches = [];
+                preg_match_all("|$regex|", $r['content'], $matches);
+                if (  isset ($matches[1]) ) {
+                    foreach ($matches[1] as $u) { $batch[] = urldecode($u); }
+                }
+            break;
+
+            // URL fields
+            default:
+                $batch[] = urldecode($r['content']);
+        }
+    }
+    return $batch;
+}
+
+function update_grackle_score(string $url, GrackleGateway $grackle)
+{
+    $file = DRUPAL_HOME.'/files'.substr($url, 40);
+    echo "\t$url\n";
+    echo "\t$file\n";
+    if (is_file($file)) {
+        $json = $grackle->scan($file);
+        if (isset($json['conformanceIndex'])) {
+            foreach ($pages as $p) {
+                $d = [
+                    'path'     => $p['alias'],
+                    'filename' => basename($internalFilename),
+                    'url'      => DRUPAL_SITE.'/sites/default/files'.$internalFilename,
+                    'score'    => (int)$json['conformanceIndex']
+                ];
+                $s = $insert->execute($d);
+                if (!$s) {
+                    $e = $webscan->errorInfo();
+                    print_r($e);
+                    print_r($d);
+                    exit();
+                }
+            }
+        }
+    }
 }
